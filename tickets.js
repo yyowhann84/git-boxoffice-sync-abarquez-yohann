d5b2d4c74e94f94d5d54bdcb5f186b0fad468fd1
Yohann <yohann.abarquez@cit.edu>
2026-09-23 14:24:06 +0800

    Add 50% VIP surcharge for premium seating - abarquez.yohann

--- tickets.js
+++ tickets.js
@@ -2,8 +2,12 @@ function isValidQuantity(quantity) {
   return quantity > 0 && quantity <= 20;
 }
 
-function calculateTicketPrice(quantity, basePrice) {
-  return Math.floor(quantity * basePrice);
+function calculateTicketPrice(quantity, basePrice, isVip = false) {
+  let total = quantity * basePrice;
+  if (isVip) {
+    total *= 1.5;
+  }
+  return Math.floor(total);
 }
 
 module.exports = { isValidQuantity, calculateTicketPrice };
